8540f1dad2251bafc59f14e4967b1e4c418ea5ac
karthikbhamidipati <bssvkarthik@gmail.com>
2021-05-13 14:04:29 +0100

    Updated model params

--- model/config.py
+++ model/config.py
@@ -29,8 +29,8 @@ NETWORKS = {
 }
 
 HYPER_PARAMETERS = {
-    'num_epochs': 20,  # number of epochs
-    'batch_size': 1024,  # batch size
+    'num_epochs': 50,  # number of epochs
+    'batch_size': 512,  # batch size
     'learning_rate': 0.01,  # learning rate
     'momentum': 0.9,  # Momentum of optimizer
     'min_learning_rate': 0.001  # minimum learning rate
