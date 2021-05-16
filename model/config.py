0a6cfdb9a8a9a62063b32da714410e00d1aa83d7
Karthik Bhamidipati <bssvkarthik@gmail.com>
2021-05-16 09:44:59 +0100

    Model training updates (#21)
    * googlenet training implementation
    
    * googlenet training implementation
    
    * batch size updated

--- model/config.py
+++ model/config.py
@@ -30,7 +30,7 @@ NETWORKS = {
 
 HYPER_PARAMETERS = {
     'num_epochs': 50,  # number of epochs
-    'batch_size': 1536,  # batch size
+    'batch_size': 2048,  # batch size
     'learning_rate': 0.02,  # learning rate
     'momentum': 0.9,  # Momentum of optimizer
     'min_learning_rate': 0.002  # minimum learning rate
